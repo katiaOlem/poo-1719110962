d1842975b68812a943467c47f1efb7d5b1ac78ca
katiaOlem <1719110962@utectulancingo.edu.mx>
2020-05-28 21:35:04 +0000

    estudiante.py Herencia Funcionando

--- semana_3/estudiante.py
+++ semana_3/estudiante.py
@@ -18,33 +18,39 @@ class Estudiante:
   #HERENCIA
 class EstudianteKatia (Estudiante):
   #ATRIBUTOS
- matricula="171911"
- carrera="Tic´s Bis"
- edad="24"
- semestre="3ero"
  promedio="9.5"
  beca="Manutencion"
- tutor="Katia"
+ tutor="Oscar"
  grupo="21"
  nivel_ingles="A3"
- estado_civil="Casada"
  #METODOS
-
- def elegir(self):
-    print("Elegir")
- def asignar(self):
-    print("Asignar")
  def mantener(self):
    print("Mantener")
  def pasar(self):
    print("Pasar")
+
  def __init__(self):
-   print("Constructor EstudianteKatia")
+   print("Constructor Estudiante Katia")
   
 objeto= Estudiante()
-objeto.elegir
-objeto.asignar
+objeto.elegir()
+objeto.asignar()
 print(objeto.nombre)
 print(objeto.matricula)
 print(objeto.carrera)
 
+objeto_estudiante_katia=EstudianteKatia()
+objeto_estudiante_katia.elegir()
+objeto_estudiante_katia.asignar()
+objeto_estudiante_katia.mantener()
+objeto_estudiante_katia.pasar()
+print(objeto_estudiante_katia.nombre)
+print(objeto_estudiante_katia.matricula)
+print(objeto_estudiante_katia.carrera)
+print(objeto_estudiante_katia.edad)
+print(objeto_estudiante_katia.semestre)
+print(objeto_estudiante_katia.promedio)
+print(objeto_estudiante_katia.beca)
+print(objeto_estudiante_katia.tutor)
+print(objeto_estudiante_katia.grupo)
+print(objeto_estudiante_katia.nivel_ingles)
