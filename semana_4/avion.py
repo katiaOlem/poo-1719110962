82f7fdbd69221a04904ae369eb19895e4b80ee8a
katiaOlem <1719110962@utectulancingo.edu.mx>
2020-06-03 21:38:40 +0000

    avion.py Funcionado Polimorfismo

---
+++ semana_4/avion.py
@@ -0,0 +1,58 @@
+class Avion():
+  #ATRIBUTOS
+  nombre="POLARIS"
+  asientos="35"
+  alas="2"
+  llantas="4"
+  ventanas="36"
+
+  #Metodos
+  def despegar(self):
+    print("Despegar")
+  def aterrizar(self):
+    print("Aterrizar")
+  def __init__(self):
+    print("Constructor Avion")
+  
+  #HERENCIA
+class AvionPolaris(Avion):
+  #ATRIBUTOS
+  color="Blanco con franjas Rojas"
+  tamano="Mediano"
+  velocidad="950 km/h"
+  altura= " 10 km "
+  control="Automatico"
+
+  #Metodos
+  def despegar(self):
+    print("Despegar de Cancun")
+  def aterrizar(self):
+    print("Aterrizar en Mexico")
+  def volar(self):
+    print("volar")
+  def manejar(self):
+    print("Manejar")
+  def __init__(self):
+    print("Constructor Avion Polaris")
+
+
+objeto_polaris=AvionPolaris()
+objeto_polaris.despegar()
+objeto_polaris.aterrizar()
+objeto_polaris.volar()
+objeto_polaris.manejar()
+objeto_polaris.nombre= " Boeing 757-200 "
+print(objeto_polaris.nombre)
+objeto_polaris.asientos= "Clase empresarial 25-  United Economy 108 - Plus United Economy 45 "
+print(objeto_polaris.asientos)
+objeto_polaris.llantas="3 llantas de doble Rodada"
+print(objeto_polaris.llantas)
+objeto_polaris.alas="1 ala volante"
+print(objeto_polaris.alas)
+objeto_polaris.ventanas="100 ventanas de resina acrílica y sintética muy resistentes "
+print(objeto_polaris.ventanas)
+print(objeto_polaris.color)
+print(objeto_polaris.tamano)
+print(objeto_polaris.velocidad)
+print(objeto_polaris.altura)
+print(objeto_polaris.control)
