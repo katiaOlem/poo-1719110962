98fd27a6bbc8e9e24aee605eaa20798b76b2cf6d
katiaOlem <1719110962@utectulancingo.edu.mx>
2020-06-04 00:11:22 +0000

    classroom.py Funcionando Polimorfismo

--- semana_4/classroom.py
+++ semana_4/classroom.py
@@ -23,10 +23,15 @@ class ClassroomMeet(Classroom):
  audio="transmmitido por Profesores y Alumnos"
 
  #METODOS
+ def calificar(self):
+   print("Calificar la tarea de los Alumnos, por los Profesores")
+ def enviar (self):
+   print("Enviar las tareas y tambien comentarios")
  def llamar(self):
-   print("Llamar")
+   print("Llamar por telefono y con la aplicacion desde celular o computadora")
  def transmitir(self):
-  print("Transmitir")
+    print("Transmitir la pantalla para todos en la videollamada")
+
  def __init__(self):
    print("Construtor Google Classroom Meet")
 
@@ -36,10 +41,15 @@ objeto_meet.calificar()
 objeto_meet.enviar()
 objeto_meet.llamar()
 objeto_meet.transmitir()
+objeto_meet.codigo="Proporsionado por la pagina de Meet"
 print(objeto_meet.codigo)
+objeto_meet.publicacion="Publicaciones para recordar las videollamadas en el grupo de Classroom"
 print(objeto_meet.publicacion)
+objeto_meet.materia="Materia creada por los Docentes"
 print(objeto_meet.materia)
+objeto_meet.alumno="Nombre completo del Alumno"
 print(objeto_meet.alumno)
+objeto_meet.tarea="Tareas asignadas por los Docentes para los alumnos"
 print(objeto_meet.tarea)
 print(objeto_meet.llamadas)
 print(objeto_meet.mensajes)
