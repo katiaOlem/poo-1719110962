022eb4c2459e64f735473b0a497fa465b1ddb67c
Katia MD <1719110962@utectulancingo.edu.mx>
2020-08-09 01:59:57 +0000

    manejo.py Funcionando

--- semana_codigos/manejo.py
+++ semana_codigos/manejo.py
@@ -1,43 +1,47 @@
-import math
+import math #Ayuda con las funciones matemáticas
 lista_numeros=[]#Contador de los números
 
 
 class NumerosError ():#Clase
     def __init__(self): #Metodo Constructor
         pass
-    def numeros_evaluar(self): #definimod un metodo para pedir los numeros
-        try:
-            numeros=int(input(" Ingrese el total de numeros que desea evaluar=     "))
+    def numeros_evaluar(self): #Metodo 
+        try:#Declaracion de Try  para identificar errores 
+            numeros=int(input(" Ingrese el total de numeros que desea evaluar =   ")) #Variable del # de  numeros que deseamos evaluar y realizar las siguientes operaciones.
 
-            for i in range (numeros):
-                numero=int(input("Ingrese el numero que desea evaluar=   "))
-                lista_numeros.append (numero)
+            for i in range (numeros):#EL rango de los  numeros
+                numero=int(input("Ingrese el numero que desea evaluar =   ")) #Variable que nos permite Ingresar los numeros a evaluar 
+                lista_numeros.append (numero) #Reemplazaremos los numeros en el contador de la lista_numeros
             
-        except Exception as error: #si detecta algun error
-            print("Verifique los datos ingresados",error.args) #imprime la siguiente linea
+        except Exception as error: # Except captura el error
+            print("!!!   ERROR   !!!",error.args) 
             
-    def Indice_numeros(self): #metodo que hara lo siguiente
+    def Indice_numeros(self): #Metodo del indice 
     
-        r="S" #ayudara al ciclo while
-        try: #ayudara a detectar un error
-            while r == "s" or otra == "S": #mietras while sea S
-                indice=int(input(" introduza un índice "))#Pide insertar un numero en de indice
-                seletion=lista_numeros[indice]#seleccionara el valor almacenado en ese indice
-                print(seletion) #imprime el valor que seleccionaste
-                raiz_cuadrada=math.sqrt(seletion)#el numero seleccionado calcula la raiz cuadrada
-                print("La raiz cuadra del numero es= "+str(raiz_cuadrada)) #imprime la raiz cuadrada
-                cubo=(seletion + seletion + seletion) #el indice selccionado lo elevera al cubo
-                print("El numero elevado al cubo es= "+str(cubo))#imprime el resultado del indice elevado al cubo
-                if seletion%2==0:#si el indice seleccionado es divicible entre dos y su reciduo final es 0
-                    print("El numero que Ingreso es PAR")#imprime la siguiente linea
-                else: #de lo contrario
-                    print("El numero que ingreso es IMPAR") #imprime lo siguiete 
-                otra=input("¿Desea realizar otro calculo? s/n ") #si desea realizar otro calculo
-                if r=="n" or r=="N": #si la respuesta es N o n
-                    break #termina el codigo
-        except Exception as error: #si detecta algun error
-            print("Verifique los datos ingresados",error.args)
+        r="s" or "S" #Respuesta S OR s 
+        try: #Declaracion de Try  para identificar errores
+            while r == "s" or r == "S": #Si la respuesta es S OR s el programa realizara Lo siguiente 
+                indice=int(input(" Introduza un índice =  "))#Variable que nos permite Ingresar el # del Indice que deseamos Imprimir con las operaciones qu estan a continuacion
+                seletion=lista_numeros[indice]#Variable que seleccionara el Indice de los numeros agregados para evaluar
+                print(seletion) #Imprimira el valor seleccionado
 
-obj=NumerosError ()
-obj.numeros_evaluar()#llamamos a nuetrso primero metodo
-obj.Indice_numeros()#llamamos a nuestro segundo metodo
+                raiz_cuadrada=math.sqrt(seletion)#Math sqrt nos permite sacar la raiz del numero seleccionado
+                print("Su Raiz Cuadrada es=  "+ str (raiz_cuadrada)) #imprime la raiz cuadrada del numero seleccionado 
+                cubo=(seletion * seletion * seletion) #El numero seleccionado se elebara al cubo
+                print("Elevado al cubo es= "+ str (cubo))#imprime el resultado elevado al cubo
+                if seletion == 0:# Si el numero es = a 0 el numero es par 
+                   print ("Este número es par=  ") #Imprime el mensaje 
+                elif seletion%2 == 0: #si el numero es divisible hasta o es numero par 
+                  print ("El numero es par") #Imprime
+                else:
+                  print ("El numero es impar ") # Si no es impar 
+                  
+                r=input(" ¿Desea realizar otro calculo? s/n ") #Varible de respuesta para saber si regresa al ciclo o termina el prgrama 
+                if r=="n" or r=="N": #si la respuesta es N o n
+                    break #Finaliza
+        except Exception as error: #Except captura el error
+            print(" !! ERROR !!",error.args) #Imprime el error 
+
+obj=NumerosError ()#Objeto de la Class 
+obj.numeros_evaluar()#Objeto del Metodo de numeros_evaluar
+obj.Indice_numeros()#Objeto del Metodo de Indice_numeros
